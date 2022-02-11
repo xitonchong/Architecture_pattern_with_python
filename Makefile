83aaaffab3868c9447a5de47d55e13a2dc61fcd1
hentai_usagi <xitonchong@gmail.com>
2022-02-11 23:19:14 +0800

    passed all tests

--- Makefile
+++ Makefile
@@ -1,14 +1,23 @@
-test:
-	pytest --tb=short 
+# these will speed up builds, for docker-compose >= 1.25
+export COMPOSE_DOCKER_CLI_BUILD=1
+export DOCKER_BUILDKIT=1
 
-watch-tests:
-	ls *.py | entr pytest --tb=short 
+all: down build up test
+
+build:
+	docker-compose build
+
+up:
+	docker-compose up -d app
+
+down:
+	docker-compose down
+
+logs:
+	docker-compose logs app | tail -100
+
+test:
+	pytest --tb=short
 
 black:
-	black -l 86 $$(find* -name '*.py')
-
-setup: requirements.txt 
-	pip install -r requirements.txt
-
-clean:
-	rm -rf __pycache__
+	black -l 86 $$(find * -name '*.py')
---
+++ services.py
@@ -0,0 +1,27 @@
+from __future__ import annotations 
+
+
+''' this file is for starting service layer to the front-end flask UI '''
+
+import model 
+from model import OrderLine 
+from repository import AbstractRepository 
+
+
+
+class InvalidSku(Exception):
+    pass
+
+
+def is_valid_sku(sku, batches):
+    return sku in {b.sku for b in batches}
+
+
+def allocate(line: OrderLine, repo: AbstractRepository, session) -> str:
+    batches = repo.list()
+    if not is_valid_sku(line.sku, batches):
+        raise InvalidSku(f"Invalid sku {line.sku}")
+    batchref = model.allocate(line, batches)
+    session.commit() 
+    return batchref 
+    
