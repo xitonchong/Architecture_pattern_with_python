42b04802179c6dbaed6ebf17b02f9673f38db864
hentai_usagi <xitonchong@gmail.com>
2022-02-21 12:27:54 +0800

    domain: init event classes

--- src/allocation/domain/events.py
+++ src/allocation/domain/events.py
@@ -1,10 +1,33 @@
 from dataclasses import dataclass 
-
+from typing import Optional 
+from datetime import date 
 
 class Event: 
     pass 
 
 
+@dataclass
+class BatchCreated(Event):
+    ref: str 
+    sku: str 
+    qty: int 
+    eta: Optional[date] = None 
+
+
+@dataclass
+class AllocationRequired(Event):
+    orderid: str 
+    sku: str 
+    qty: int 
+
+
+@dataclass
+class BatchQuantityChanged(Event):
+    ref: str 
+    qty: int 
+
+
 @dataclass 
 class OutOfStock(Event):
     sku: str 
+
