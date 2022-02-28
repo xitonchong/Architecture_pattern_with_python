1980f736ed2145680c9bfb91e0ede6fd5e1dfd40
hentai_usagi <xitonchong@gmail.com>
2022-02-28 14:54:11 +0800

    abstraction on notification/email system

---
+++ src/allocation/adapters/notifications.py
@@ -0,0 +1,27 @@
+# pylint: disable=too-few-public-methods
+import abc 
+import smtplib
+from allocation import config
+
+
+class AbstractNotification(abc.ABC):
+    @abc.abstractmethod
+    def send(self, destination, message):
+        raise NotImplementedError
+
+DEFAULT_HOST = config.get_email_host_and_port()["host"]
+DEFAULT_PORT = config.get_email_host_and_port()["port"]
+
+class EmailNotifications(AbstractNotification):
+    def __init__(self, smtp_host=DEFAULT_HOST, port=DEFAULT_PORT):
+        self.server = smtplib.SMTP(smtp_host, port=port)
+        self.server.noop() 
+
+
+    def send(self, destination, message):
+        msg = f"Subject: allocation service notification\n {message}"
+        self.server.sendmail(
+            from_addr="allocation@example.com",
+            to_addrs=[destination],
+            msg=msg,
+        )
